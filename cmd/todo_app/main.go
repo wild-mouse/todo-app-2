130d5d04a7d5e068a13c8e6cb9758338bcce4d69
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-11-12 20:25:54 +0900

    Add simple CRUD endpoints

--- cmd/todo_app/main.go
+++ cmd/todo_app/main.go
@@ -7,8 +7,31 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/tasks", getTasks)
+	e.GET("/tasks/:id", getTask)
+	e.POST("/tasks", saveTask)
+	e.PUT("/tasks/:id", updateTask)
+	e.DELETE("/tasks/:id", deleteTask)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getTasks(c echo.Context) error {
+	return c.String(http.StatusOK, "This endpoint should returns tasks.")
+}
+
+func getTask(c echo.Context) error {
+	return c.String(http.StatusOK, "This endpoint should returns single task.")
+}
+
+func saveTask(c echo.Context) error {
+	return c.String(http.StatusOK, "This endpoint should save single task.")
+}
+
+func updateTask(c echo.Context) error {
+	return c.String(http.StatusOK, "This endpoint should update single task.")
+}
+
+func deleteTask(c echo.Context) error {
+	return c.String(http.StatusOK, "This endpoint should delete single task.")
 }
